8a23d916d6ca05c63d340b0d98194c811fd880aa
Taro Furuya <taro.furuya@gmail.com>
2017-08-05 16:50:43 +0900

    chap3

--- mysite/helloworld/models.py
+++ mysite/helloworld/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Add_Word(models.Model):
+    word_text = models.CharField(max_length=140)
+    date_time = models.DateTimeField('保存日')
