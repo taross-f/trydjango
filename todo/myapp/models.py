65ac141e9f3b1e8a9a7e3d90f372e460ab5cb668
Taro Furuya <taro.furuya@gmail.com>
2017-08-06 01:24:42 +0900

    end

--- todo/myapp/models.py
+++ todo/myapp/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class TodoBoard(models.Model):
     new_message = models.CharField(max_length=200,)
+    image_url = models.ImageField(upload_to='images/',null=True)
     created_at = models.DateTimeField(auto_now_add=True,)
--- todo/myapp/views.py
+++ todo/myapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import TodoBoard
 
 def myapp_index(request):
     delete_text = request.POST.getlist('delete_text')
     if delete_text:
         TodoBoard.objects.filter(id__in=delete_text).delete()
-    msg = request.GET.get('words')
+    msg = request.POST.get('words')
     if msg is None or msg == "":
         data_list = TodoBoard.objects.order_by('-id')
         contexts = {
@@ -13,8 +14,10 @@ def myapp_index(request):
         }
         return render(request, 'myapp/index.html', contexts)
 
+    image_file = request.FILES.get('files')
     message_data = TodoBoard()
     message_data.new_message = msg
+    message_data.image_url = image_file
     message_data.save()
 
     data_list = TodoBoard.objects.order_by('-id')
